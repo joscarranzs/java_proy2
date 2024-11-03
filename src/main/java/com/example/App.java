a51f2e2ef6454c0f2933221be7df7bfd08464680
albertocarranza <albertososacarranza@gmail.com>
2024-11-02 21:58:13 -0500

    Creation of the main menu interface

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.graphic_interface.MainMenu;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) {      
+      MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
+        mainMenu.setLocationRelativeTo(null);
   }
 }
