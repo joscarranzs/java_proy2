77aca1ad3def2a933486dcafff85fd70725fc06d
joscarranzs <jcarranzsosa@gmail.com>
2024-11-04 19:07:38 -0500

    Indented code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,8 +4,8 @@ import com.graphic_interface.MainMenu;
 
 public class App {
   public static void main(String[] args) {      
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.setVisible(true);
-        mainMenu.setLocationRelativeTo(null);
+    MainMenu mainMenu = new MainMenu();
+    mainMenu.setVisible(true);
+    mainMenu.setLocationRelativeTo(null);
   }
 }
--- src/main/java/com/example/validates/ValidatesProgram1.java
+++ src/main/java/com/example/validates/ValidatesProgram1.java
@@ -11,19 +11,19 @@ import javax.swing.JOptionPane;
  * @author Alberto
  */
 public class ValidatesProgram1 {
-    public static boolean validatePositive(int number){
-        if(number <=0){
-            JOptionPane.showMessageDialog(null, "Error. Ingrese valores positivos.", null, JOptionPane.ERROR_MESSAGE);
-            return true;
-        }
-        return false;
-     }
-    
-    public static boolean validateEmptyValues(String value){
-        if(value.isEmpty()){
-             JOptionPane.showMessageDialog(null, "Los valores no pueden estar vacios", null, JOptionPane.ERROR_MESSAGE);
-             return true;
-        }
-        return false;
+  public static boolean validatePositive(int number){
+    if(number <=0){
+      JOptionPane.showMessageDialog(null, "Error. Ingrese valores positivos.", null, JOptionPane.ERROR_MESSAGE);
+      return true;
     }
+    return false;
+  }
+
+  public static boolean validateEmptyValues(String value){
+    if(value.isEmpty()){
+      JOptionPane.showMessageDialog(null, "Los valores no pueden estar vacios", null, JOptionPane.ERROR_MESSAGE);
+      return true;
+    }
+    return false;
+  }
 }
